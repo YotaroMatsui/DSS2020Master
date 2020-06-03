395225cb1b47c407fde54b8fdbf71f4170f91bad
Kohei Watanabe <43807194+Kohei1023@users.noreply.github.com>
2020-06-03 21:08:14 +0900

    Add files via upload

--- src/Term2/lightGBM.py
+++ src/Term2/lightGBM.py
@@ -1,8 +1,4 @@
-# -*- coding: utf-8 -*-
-
 import lightgbm as lgb
-#from lightgbm import LGBMRegressor as lbg
-
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
@@ -10,9 +6,6 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 import pandas as pd
 
-"""LightGBM を使った回帰のサンプルコード"""
-
-# データセットを読み込む
 df_x = pd.read_csv('../../resource/Term2/train_processed.csv') # x:説明変数
 df_test = pd.read_csv('../../resource/Term2/test_processed.csv')  # testデータ
 
@@ -31,7 +24,7 @@ df_x = df_x.drop('id', axis=1)
 # 訓練データとテストデータに分割する
 X_train, X_test, y_train, y_test = train_test_split(df_x, df_y, test_size=2)
 
-# 上記のパラメータでモデルを学習する
+# モデルを学習する
 model = lgb.LGBMRegressor()
 model.fit(X_train, y_train)
 
@@ -56,9 +49,6 @@ y_pred = model.predict(df_test)
 y_pred = [int(n) for n in y_pred]
 #print(len(y_pred))
 df = pd.DataFrame(y_pred)
-
-
-
 
 df = pd.concat([df_id, df], axis=1)
 #print(df)
